24315d6c808951e2a56f637ac5c2e0074cee4af8
Bayu Prayoga <106808389+dbestbay@users.noreply.github.com>
2022-06-03 19:59:04 +0700

    Create Create.blade.php

---
+++ Create.blade.php
@@ -0,0 +1,61 @@
+@extends('template')
+
+@section('content')
+<div class="row mt-5 mb-5">
+    <div class="col-lg-12 margin-tb">
+        <div class="float-left">
+            <h2>Create New Siswa</h2>
+        </div>
+        <div class="float-right">
+            <a class="btn btn-secondary" href="{{ route('sisw.index') }}"> Kembali</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> Input gagal.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form action="{{ route('sisw.store') }}" method="POST">
+    @csrf
+
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>NIK:</strong>
+                <input type="text" name="NIS" class="form-control" placeholder="NIK">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama:</strong>
+                <input type="text" name="Nama" class="form-control" placeholder="NAMA WARGA">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Jenkel:</strong>
+                <input type="radio" value="Laki-Laki" name="Laki-Laki" class="form-control">
+                <Input type="radio" value="Perempuan" name="Laki-Laki" class="form-control">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Alamat:</strong>
+                <textarea class="form-control" style="height:150px" name="Alamat" placeholder="Content"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+
+</form>
+@endsection
